284f7c6d09f25327b1e70bff9418f4ae0643e3c9
Isabelle Thorpe <isabelle_thorpe@Outlook.com>
2022-04-28 16:00:36 +0100

    added user table with test data

---
+++ db/seeds/UserSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+
+use Phinx\Seed\AbstractSeed;
+
+class UserSeeder extends AbstractSeed
+{
+    /**
+     * Run Method.
+     *
+     * Write your database seeder using this method.
+     *
+     * More information on writing seeders is available here:
+     * https://book.cakephp.org/phinx/0/en/seeding.html
+     */
+    public function run()
+    {
+        $data = [
+            [
+                'email'    => 'nigellalawson@microwave.com',
+                'mobile'    => '07777777666',
+                'first_name'    => 'Nigella',
+                'last_name'    => 'Lawson',
+                'password'      => password_hash('password', PASSWORD_DEFAULT),
+                'user_type'    => 'admin',
+                'email_notifications'    => 1,
+                'sms_notifications'    => 1,
+                'created' => date('Y-m-d H:i:s'),
+            ]
+        ];
+
+        $users = $this->table('users');
+        $users->insert($data)
+              ->saveData();
+    }
+}

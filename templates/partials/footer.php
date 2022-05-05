abd9f21bf87e5a28a8350d286119f5d1eb51eccc
AtishAppadu <wuxiashia@gmail.com>
2022-05-05 12:08:32 +0100

    updated footer

--- templates/partials/footer.php
+++ templates/partials/footer.php
@@ -1,8 +1,31 @@
-<body class="d-flex flex-column min-vh-100">
 
-<div class="text-white mb-3 mb-md-0">
-      Copyright © 2022. Meal planner App
+
+
+
+  <div
+    class="d-flex flex-column flex-md-row text-center text-md-start justify-content-between py-4 px-4 px-xl-5 bg-primary">
+    <div class="text-white mb-3 mb-md-0">
+      Copyright © 2022. Meal planner App.
     </div>
+
+    <!-- Right -->
+    <div>
+      <a href="#!" class="text-white me-4">
+        <i class="fab fa-facebook-f"></i>
+      </a>
+      <a href="#!" class="text-white me-4">
+        <i class="fab fa-twitter"></i>
+      </a>
+      <a href="#!" class="text-white me-4">
+        <i class="fab fa-google"></i>
+      </a>
+      <a href="#!" class="text-white">
+        <i class="fab fa-linkedin-in"></i>
+      </a>
+    </div>
+    <!-- Right -->
+  </div>
+
 
 </div>
 

116da4bba6dda779dbad7715f1142bb1633b0157
Isabelle Thorpe <isabelle_thorpe@Outlook.com>
2022-05-12 12:47:47 +0100

    updated details script to update details - however this needs to be in edit details and details have its own script to pull from db

--- details.php
+++ details.php
@@ -15,23 +15,24 @@ if (isset($_SESSION['id'])) {
    if (isset($_POST['submit'])) {
 
       // build an sql statment to update the student details
-      $sql = "update student set firstname ='" . $_POST['txtfirstname'] . "',";
-      $sql .= "lastname ='" . $_POST['txtlastname']  . "',";
-      $sql .= "house ='" . $_POST['txthouse']  . "',";
-      $sql .= "town ='" . $_POST['txttown']  . "',";
-      $sql .= "county ='" . $_POST['txtcounty']  . "',";
-      $sql .= "country ='" . $_POST['txtcountry']  . "',";
-      $sql .= "postcode ='" . $_POST['txtpostcode']  . "' ";
-      $sql .= "where studentid = '" . $_SESSION['id'] . "';";
+      $sql = "UPDATE users set first_name ='" . $_POST['txtfirstname'] . ";";
+      $sql .= "last_name ='" . $_POST['txtlastname']  . "',";
+      $sql .= "email ='" . $_POST['txtemail']  . "',";
+      $sql .= "mobile ='" . $_POST['txtmobile']  . "',";
+      $sql .= "email_notifications ='" . $_POST['txtemailnotifications']  . "',";
+      $sql .= "sms_notifications ='" . $_POST['txtsmsnotifications']  . "',";
+      $sql .= "password ='" . $_POST['txtpassword']  . "' ";
+      $sql .= "where id = '" . $_SESSION['id'] . "';";
       $result = mysqli_query($conn,$sql);
 
-      $data['content'] = "<p>Your details have been updated</p>";
+      // boostrap confirmation message
+      $data['content'] .= "<div class='alert alert-success' role='alert'>Details updated successfully</div>";
 
    }
    else {
       // Build a SQL statment to return the student record with the id that
       // matches that of the session variable.
-      $sql = "select * from student where studentid='". $_SESSION['id'] . "';";
+      $sql = "SELECT * from users where id='". $_SESSION['id'] . "';";
       $result = mysqli_query($conn,$sql);
       $row = mysqli_fetch_array($result);
 
@@ -42,20 +43,21 @@ if (isset($_SESSION['id'])) {
 
    <h2>My Details</h2>
    <form name="frmdetails" action="" method="post">
+
    First Name :
-   <input name="txtfirstname" type="text" value="{$row['firstname']}" /><br/>
+   <input name="txtfirstname" type="text" value="{$row['first_name']}" /><br/>
    Surname :
-   <input name="txtlastname" type="text"  value="{$row['lastname']}" /><br/>
-   Number and Street :
-   <input name="txthouse" type="text"  value="{$row['house']}" /><br/>
-   Town :
-   <input name="txttown" type="text"  value="{$row['town']}" /><br/>
-   County :
-   <input name="txtcounty" type="text"  value="{$row['county']}" /><br/>
-   Country :
-   <input name="txtcountry" type="text"  value="{$row['country']}" /><br/>
-   Postcode :
-   <input name="txtpostcode" type="text"  value="{$row['postcode']}" /><br/>
+   <input name="txtlastname" type="text"  value="{$row['last_name']}" /><br/>
+   Email :
+   <input name="txtemail" type="text"  value="{$row['email']}" /><br/>
+   Mobile :
+   <input name="txtmobile" type="text"  value="{$row['mobile']}" /><br/>
+   Email Notifications :
+   <input name="txtemailnotifications" type="text"  value="{$row['email_notifications']}" /><br/>
+   SMS Notifications :
+   <input name="txtsmsnotifications" type="text"  value="{$row['sms_notifications']}" /><br/>
+   Password :
+   <input name="txtpassword" type="text"  value="{$row['password']}" /><br/>
    <input type="submit" value="Save" name="submit"/>
    </form>
 

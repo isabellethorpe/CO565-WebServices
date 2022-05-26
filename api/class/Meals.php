f04ddc6f82c0ef842acc36d0846d5e8e6748633c
Isabelle Thorpe <isabelle_thorpe@Outlook.com>
2022-05-26 10:37:41 +0100

    commented out create/update/delete from meals

---
+++ api/class/Meals.php
@@ -0,0 +1,71 @@
+<?php
+
+class Meals 
+{
+    
+    function __construct($db) {
+        $this->conn = $db;
+    }
+
+    function read(){	
+        if($this->id) {
+            $stmt = $this->conn->prepare("SELECT * FROM meals WHERE id = ?");
+            $stmt->bind_param("i", $this->id);					
+        } else {
+            $stmt = $this->conn->prepare("SELECT * FROM meals");		
+        }		
+        $stmt->execute();			
+        $result = $stmt->get_result();		
+        return $result;	
+    }
+
+    // function create() {
+    //     $stmt = $this->conn->prepare("INSERT INTO meals (`name`, `description`, `price`, `category_id`, `created`) VALUES (?,?,?,?,?)");
+        
+    //     $this->name = htmlspecialchars(strip_tags($this->name));
+    //     $this->description = htmlspecialchars(strip_tags($this->description));
+    //     $this->price = htmlspecialchars(strip_tags($this->price));
+    //     $this->category_id = htmlspecialchars(strip_tags($this->category_id));
+    //     $this->created = htmlspecialchars(strip_tags($this->created));
+        
+    //     $stmt->bind_param("ssiis", $this->name, $this->description, $this->price, $this->category_id, $this->created);
+        
+    //     if($stmt->execute()) {
+    //         return true;
+    //     }
+    //     return false;		 
+    // }
+
+    // function update(){
+	 
+    //     $stmt = $this->conn->prepare("UPDATE meals SET name= ?, description = ?, price = ?, category_id = ?, created = ? WHERE id = ?");
+     
+    //     $this->id = htmlspecialchars(strip_tags($this->id));
+    //     $this->name = htmlspecialchars(strip_tags($this->name));
+    //     $this->description = htmlspecialchars(strip_tags($this->description));
+    //     $this->price = htmlspecialchars(strip_tags($this->price));
+    //     $this->category_id = htmlspecialchars(strip_tags($this->category_id));
+    //     $this->created = htmlspecialchars(strip_tags($this->created));
+     
+    //     $stmt->bind_param("ssiisi", $this->name, $this->description, $this->price, $this->category_id, $this->created, $this->id);
+        
+    //     if($stmt->execute()) {
+    //         return true;
+    //     }
+    //     return false;
+    // }
+
+    // function delete(){
+		
+    //     $stmt = $this->conn->prepare("DELETE FROM meals WHERE id = ?");
+    //     $this->id = htmlspecialchars(strip_tags($this->id));
+    //     $stmt->bind_param("i", $this->id);
+     
+    //     if($stmt->execute()){
+    //         return true;
+    //     }
+    //     return false;		 
+    // }
+    
+    
+}
